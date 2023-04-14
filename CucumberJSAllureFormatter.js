a5879809319f2dc4cae06574edbfb1a553591626
Bruno Figueiredo <brunonf15@gmail.com>
2023-04-14 16:10:05 +0100

    check if allure-results folder is already created if not create

--- CucumberJSAllureFormatter.js
+++ CucumberJSAllureFormatter.js
@@ -1,9 +1,16 @@
 const { CucumberJSAllureFormatter } = require("allure-cucumberjs");
 const { AllureRuntime } = require("allure-js-commons");
+const fs = require("fs");
 
 class AllureReporter extends CucumberJSAllureFormatter {
   constructor(options) {
     const allure = new AllureRuntime({ resultsDir: "allure-results" });
+
+    // Check if "allure-results" directory exists, if not, create it
+    if (!fs.existsSync("allure-results")) {
+      fs.mkdirSync("allure-results");
+    }
+
     super(options, allure);
   }
 }
