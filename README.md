a4bb9c661ced9a392b970062e253077bd5869e92
Bruno Figueiredo <brunonf15@gmail.com>
2023-04-14 15:32:39 +0100

    2 first steps

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Introduction
 This is a test automation project built with Cucumber.js and Playwright. It contains end-to-end tests for a web application.
 
-## Pre-requisites
+## Pre-requirements
 Before running this project, you need to install Node.js. You can download and install Node.js from the official website https://nodejs.org/en/.
 
 To verify that Node.js is installed, open a terminal and run the following command:
@@ -21,8 +21,6 @@ npm i
 
 This command will install all the dependencies listed in the package.json file. If you encounter any errors, you may need to install Cucumber and Playwright globally.
 
-If you want to generate reports using Allure, you also need to have Java installed on your computer.
-
 Running Tests
 To run all the tests, type the following command in the terminal:
 
@@ -32,16 +30,16 @@ npm run all
 
 This command will run all the .feature files in the features directory.
 
-To run a specific test, you can use the @only tag. Add the tag above the scenario you want to run, and then use the following command in the terminal:
-
-```bash
-npm run only
-```
-
 To run a test and generate the data needed to create the Allure report, use the following command, but you would have Java installed on your machine:
 
 ```bash
 npm run testWithAllureReport
+```
+
+If you want to generate reports using Allure, you also need to have Java installed on your computer.
+
+```bash
+npm run allure
 ```
 
 To view the Allure report, use the following command:
@@ -50,9 +48,32 @@ To view the Allure report, use the following command:
 npm run allure
 ```
 
-To run over Firefox or webkit you need to update a enviroment variable, you can try for example:
+To run over Firefox or webkit you need to update a enviroment variable, that is chromium by default, you can try for example:
 
 ```bash
+export BROWSER=chromium
+
 export BROWSER=firefox
+
+export BROWSER=webkit
+
 ```
 
+If you want to enable or disable the Playwright debugger, you can try for example:
+
+```bash
+
+export PWDEBUG=1
+
+export PWDEBUG=0
+
+```
+
+If you want to enable or disable the headless mode, that is disabled by defaul, you can try for example:
+
+```bash
+
+export HEADLESS=true
+
+export HEADLESS=false
+```
--- features/step_definitions/form.steps.js
+++ features/step_definitions/form.steps.js
@@ -1,177 +1,62 @@
 const { Given, When, Then, AfterAll, BeforeAll } = require('@cucumber/cucumber');
 const { chromium, firefox, webkit } = require('playwright');
 const { expect } = require('chai');
+const path = require('path');
 
 let browser;
 let context;
 let page;
-let dialogMessage = '';
+let headless = process.env.HEADLESS !== 'false';
+const browserName = process.env.BROWSER || 'chromium';
 
 BeforeAll(async () => {
-  browserName = process.env.BROWSER || 'chromium'; // Access the browser environment variable or use 'chromium' as default
+  // Access the browser environment variable or use 'chromium' as default
+
   if (browserName === 'chromium') {
-    browser = await chromium.launch({ headless: false });
+    browser = await chromium.launch({ headless });
   } else if (browserName === 'firefox') {
-    browser = await firefox.launch({ headless: false });
+    browser = await firefox.launch({ headless });
   } else if (browserName === 'webkit') {
-    browser = await webkit.launch({ headless: false });
+    browser = await webkit.launch({ headless });
   } else {
     throw new Error(`Invalid browser name: ${browserName}`);
   }
-  
+
   context = await browser.newContext();
   page = await context.newPage();
 });
 
-Given('Open Mercedes-benz United Kingdom market', { timeout: 15000 }, async () => {
+Given('Open Mercedes-benz United Kingdom market', { timeout: 60000 }, async () => {
+
+  await page.setViewportSize({ width: 1280, height: 820 }); // Set window size
+
+  // Visit website
   await page.goto('https://www.mercedes-benz.co.uk/');
+
+  const pageTitle = await page.title(); // Retrieve page title
+  expect(pageTitle).to.equal('Mercedes-Benz Passenger Cars'); //Check the title
+
+  // Wait for the Agree to all element to be available
+  await page.waitForSelector('div > div > div.cmm-cookie-banner__content > cmm-buttons-wrapper > div > div > button.wb-button.wb-button--primary.wb-button--small.wb-button--accept-all', { timeout: 12000 });
+
+  // Click on the Agree to all element
+  await page.click('div > div > div.cmm-cookie-banner__content > cmm-buttons-wrapper > div > div > button.wb-button.wb-button--primary.wb-button--small.wb-button--accept-all');
+
 });
+
+
+When('Under “Our Models” - Select “Model: Hatchbacks”', { timeout: 60000 }, async () => {
+
+  //Map and go to Hatchbacks button
+  await page.$eval('button:has-text("Hatchbacks")', (element) => element.scrollIntoViewIfNeeded());
+
+  // Click on the element
+  await page.click('button:has-text("Hatchbacks")');
+
+});
+
 
 AfterAll(async () => {
   // Close the browser after the tests have completed
   await browser.close();
-});
-
-When('I enter valid form data', async () => {
-  const data = {
-    FirstName: 'John',
-    LastName: 'Doe',
-    Email: 'johndoe@example.com',
-    PhoneNumber: '1234567890',
-    Gender: 'Male',
-    Agreement: true,
-  };
-  await page.fill('input[name="FirstName"]', data.FirstName);
-  await page.fill('input[name="LastName"]', data.LastName);
-  await page.fill('input[name="Email"]', data.Email);
-  await page.fill('input[name="PhoneNumber"]', data.PhoneNumber);
-  await page.check(`input[type="radio"][value="${data.Gender}"]`);
-  await page.check('input[type="checkbox"][name="Agreement"]');
-});
-
-When('I click the Submit button', async () => {
-  await Promise.all([
-    page.on('dialog', async (dialog) => {
-      dialogMessage = await dialog.message();
-      await dialog.accept();
-    }),
-    page.click('input[type="submit"][name="submitbutton"]'),
-  ]);
-});
-
-Then('I should assert the success message', async () => {
-  const expectedData = {
-    FirstName: 'John',
-    LastName: 'Doe',
-    Email: 'johndoe@example.com',
-    PhoneNumber: '1234567890',
-    Gender: 'Male',
-    Agreement: true,
-  };
-  const expectedMessage = JSON.stringify(expectedData);
-  expect(dialogMessage).to.contain(expectedMessage);
-  console.log('Form submitted successfully');
-  await browser.close();
-});
-
-When('I enter incomplete form data', async () => {
-  await page.check('input[type="checkbox"][name="Agreement"]');
-});
-
-Then('I should assert that no have message', async () => {
-  expect(dialogMessage).to.contain('');
-  console.log('Form submitted successfully');
-  await browser.close();
-});
-
-When('I enter an invalid First name', async () => {
-  const data = {
-    FirstName: '1',
-    LastName: 'Doe',
-    Email: 'johndoe@example.com',
-    PhoneNumber: '1234567890',
-    Gender: 'Male',
-    Agreement: true,
-  };
-  await page.fill('input[name="FirstName"]', data.FirstName);
-  await page.fill('input[name="LastName"]', data.LastName);
-  await page.fill('input[name="Email"]', data.Email);
-  await page.fill('input[name="PhoneNumber"]', data.PhoneNumber);
-  await page.check(`input[type="radio"][value="${data.Gender}"]`);
-  await page.check('input[type="checkbox"][name="Agreement"]');
-});
-
-When('I should assert that a valid First name is required', async () => {
-  const errorMessage = 'Valid first name is required.';
-  const errorSelector = 'text=Valid first name is required.';
-  const errorElement = await page.waitForSelector(errorSelector);
-  const errorText = await errorElement.textContent();
-  expect(errorText).contain(errorMessage);
-});
-
-When('I enter an invalid Phone number', async () => {
-  const data = {
-    FirstName: 'bruno',
-    LastName: 'Doe',
-    Email: 'johndoe@example.com',
-    PhoneNumber: 'fff',
-    Gender: 'Male',
-    Agreement: true,
-  };
-  await page.fill('input[name="FirstName"]', data.FirstName);
-  await page.fill('input[name="LastName"]', data.LastName);
-  await page.fill('input[name="Email"]', data.Email);
-  await page.fill('input[name="PhoneNumber"]', data.PhoneNumber);
-  await page.check(`input[type="radio"][value="${data.Gender}"]`);
-  await page.check('input[type="checkbox"][name="Agreement"]');
-});
-
-When('I enter an valid Phone number', async () => {
-  const data = {
-    FirstName: 'Bruno',
-    LastName: 'Doe',
-    Email: 'johndoe@example.com',
-    PhoneNumber: '+351915444876',
-    Gender: 'Male',
-    Agreement: true,
-  };
-  await page.fill('input[name="FirstName"]', data.FirstName);
-  await page.fill('input[name="LastName"]', data.LastName);
-  await page.fill('input[name="Email"]', data.Email);
-  await page.fill('input[name="PhoneNumber"]', data.PhoneNumber);
-  await page.check(`input[type="radio"][value="${data.Gender}"]`);
-  await page.check('input[type="checkbox"][name="Agreement"]');
-});
-
-When('I should assert that a valid Phone number is required', async () => {
-  const errorMessage = 'Valid phone number is required.';
-  const errorSelector = 'text=Valid phone number is required.';
-  const errorElement = await page.waitForSelector(errorSelector);
-  const errorText = await errorElement.textContent();
-  expect(errorText).contain(errorMessage);
-});
-
-When('I enter an invalid email', async () => {
-  const data = {
-    FirstName: 'bruno',
-    LastName: 'Doe',
-    Email: 'testinvalid',
-    PhoneNumber: '129312931',
-    Gender: 'Male',
-    Agreement: true,
-  };
-  await page.fill('input[name="FirstName"]', data.FirstName);
-  await page.fill('input[name="LastName"]', data.LastName);
-  await page.fill('input[name="Email"]', data.Email);
-  await page.fill('input[name="PhoneNumber"]', data.PhoneNumber);
-  await page.check(`input[type="radio"][value="${data.Gender}"]`);
-  await page.check('input[type="checkbox"][name="Agreement"]');
-});
-
-When('I should assert that a valid email is required', async () => {
-  const errorMessage = 'Valid email is required.';
-  const errorSelector = 'text=Valid email is required.';
-  const errorElement = await page.waitForSelector(errorSelector);
-  const errorText = await errorElement.textContent();
-  expect(errorText).contain(errorMessage);
 });
